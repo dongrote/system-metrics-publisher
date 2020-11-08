3788c72ec040e7d04d47330b278e8d0c3205577b
Don Grote <don.grote@gmail.com>
2020-11-08 14:08:11 -0500

    wip

--- service/core/Processor/ProcessorInterface.js
+++ service/core/Processor/ProcessorInterface.js
@@ -7,6 +7,7 @@ exports = module.exports = ProcessorInterface;
 ProcessorInterface.prototype.queryFrequencies = async function () {
   const procs = [];
   for (const proc in this.processors) {
+    console.dir(proc);
     procs.push({
       cpu: proc.cpu(),
       min: proc.minimumFrequency(),
