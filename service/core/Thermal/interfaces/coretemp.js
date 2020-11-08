a8c639c14d5eb792d03a7ee89bc73b073ba9fecb
Don Grote <don.grote@gmail.com>
2020-11-08 15:44:45 -0500

    wip

--- service/core/Thermal/interfaces/coretemp.js
+++ service/core/Thermal/interfaces/coretemp.js
@@ -2,7 +2,7 @@
 const _ = require('lodash'),
   ThermalInterface = require('../ThermalInterface');
 
-const thermalFilesDirectory = '/sys/devices/platform/coretemp.0/hwmon/hwmon0/';
+const hwmonDirectory = '/sys/devices/platform/coretemp.0/hwmon';
 
 function CoretempThermalInterface (options) {
   ThermalInterface.call(this, options);
@@ -23,26 +23,29 @@ const fs = require('fs-extra'),
 
 CoretempThermalInterface.create = async () => {
   try {
-    const thermalDirectoryEntries = await fs.readdir(thermalFilesDirectory);
-    const thermalzonecount = thermalDirectoryEntries.filter(fname => fname.startsWith('temp') && fname.endsWith('_input')).length;
-    const thermalFileDescriptors = [];
-    for (var i = 0; i < thermalzonecount; i++) {
-      thermalFileDescriptors.push({
-        input: `temp${i + 1}_input`,
-        label: `temp${i + 1}_label`,
-        crit: `temp${i + 1}_crit`,
-      });
-    }
     const thermalFiles = [];
-    for (const thermalFileDescriptor of thermalFileDescriptors) {
-      const nameBuffer = await fs.readFile(path.join(thermalFilesDirectory, thermalFileDescriptor.label));
-      const criticalBuffer = await fs.readFile(path.join(thermalFilesDirectory, thermalFileDescriptor.crit));
-      const inputPath = path.join(thermalFilesDirectory, thermalFileDescriptor.input);
-      thermalFiles.push(new ThermalFile(
-        nameBuffer.toString().trim(),
-        inputPath,
-        Number(criticalBuffer.toString()) / 1000
-      ));
+    const hwmonDirectories = await fs.readdir(hwmonDirectory);
+    for (const hwmon of hwmonDirectories) {
+      const thermalDirectoryEntries = await fs.readdir(path.join(hwmonDirectory, hwmon));
+      const thermalzonecount = thermalDirectoryEntries.filter(fname => fname.startsWith('temp') && fname.endsWith('_input')).length;
+      const thermalFileDescriptors = [];
+      for (var i = 0; i < thermalzonecount; i++) {
+        thermalFileDescriptors.push({
+          input: `temp${i + 1}_input`,
+          label: `temp${i + 1}_label`,
+          crit: `temp${i + 1}_crit`,
+        });
+      }
+      for (const thermalFileDescriptor of thermalFileDescriptors) {
+        const nameBuffer = await fs.readFile(path.join(hwmonDirectory, hwmon, thermalFileDescriptor.label));
+        const criticalBuffer = await fs.readFile(path.join(hwmonDirectory, hwmon, thermalFileDescriptor.crit));
+        const inputPath = path.join(hwmonDirectory, hwmon, thermalFileDescriptor.input);
+        thermalFiles.push(new ThermalFile(
+          nameBuffer.toString().trim(),
+          inputPath,
+          Number(criticalBuffer.toString()) / 1000
+        ));
+      }  
     }
     return new CoretempThermalInterface({thermalFiles});
   } catch (e) {
