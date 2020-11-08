896bc2d18d21fb4f10d22338a37107ac1f86dcfb
Don Grote <don.grote@gmail.com>
2020-11-08 12:35:46 -0500

    wip

--- service/core/Thermal/findInterface.js
+++ service/core/Thermal/findInterface.js
@@ -2,6 +2,7 @@
 const interfaces = require('./interfaces');
 exports = module.exports = async () => {
   for (const name in interfaces) {
+    console.dir(interfaces[name]);
     const i = await interfaces[name].create();
     if (i) {
       return i;
